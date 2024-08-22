363023a08d288e02c722fbc7d5df03446fb334b4
kewablay <kojokewajunior123@gmail.com>
2024-08-22 16:37:14 +0000

    cleaned up app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataService } from './services/dataService/data.service';
+import { ProductListComponent } from "./components/product-list/product-list.component";
+import { CartComponent } from "./components/cart/cart.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductListComponent, CartComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
 export class AppComponent {
   title = 'product-list-with-cart';
+  products: any = [];
+
+  constructor(private data: DataService) {
+    // this.data.getData().subscribe((data) => {
+    //   this.products = data;
+    //   console.log('Recieved Products: ', this.products);
+    // });
+  }
+  
 }
