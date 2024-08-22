b172a93f096f258238601214abd1ea37119ec6d2
kewablay <kojokewajunior123@gmail.com>
2024-08-22 16:37:32 +0000

    added configuration for store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,28 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore, provideState } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { productsReducer } from './store/products/reducers/products.reducers';
+import { ProductsEffects } from './store/products/effects/products.effects';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideStore(),
+    provideEffects(ProductsEffects),
+    provideState({
+      name: 'products',
+      reducer: productsReducer,
+    }),
+    provideHttpClient(),
+    // provideState({
+    //   name: 'cart',
+    //   reducer: cartReducer,
+    // }),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+  ],
 };
