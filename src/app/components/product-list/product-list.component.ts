f5182d495a73f9ab514bf2423423cd61bc1a2832
kewablay <kojokewajunior123@gmail.com>
2024-08-22 16:39:18 +0000

    added functionality for getting data from store

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Products } from '../../models/product.model';
+import { DataService } from '../../services/dataService/data.service';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../store/app.state';
+import { loadProducts } from '../../store/products/actions/products.actions';
+import { selectProducts } from '../../store/products/selectors/products.selectors';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.sass'
+  styleUrl: './product-list.component.sass',
 })
 export class ProductListComponent {
+  products$!: Observable<Products[]>;
+  loading$!: Observable<boolean>;
 
+  constructor(private store: Store<AppState>) {
+    console.log('Product list component created');
+    // this.products$ = this.store.select((state) => state.products.products);
+    this.products$ = this.store.select(selectProducts);
+    this.store
+      .select('products')
+      .subscribe((data) => console.log('data: ', data)); // ;
+    // this.loading$ = this.store.select((state) => state.products.loading);
+    console.log('Products: ', this.products$);
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(loadProducts());
+    // Subscribe to products$ observable
+    this.products$.subscribe((products) => {
+      console.log('Products in oninit:', products);
+    });
+  }
 }
