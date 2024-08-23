cce9fd626d9c706b079a24ea87ac8263b97982ee
kewablay <kojokewajunior123@gmail.com>
2024-08-23 07:49:51 +0000

    added local storage service

---
+++ src/app/services/localStorageService/local-storage.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocalStorageService {
+  constructor() {}
+
+  setItem(key: string, value: any): void {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  getItem(key: string): any {
+    const data = localStorage.getItem(key);
+    return data ? JSON.parse(data) : null;
+  }
+}
