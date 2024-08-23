7f8216f00083bb9afdad0ff21fe833caccb635ad
kewablay <kojokewajunior123@gmail.com>
2024-08-23 09:39:37 +0000

    added place order to cart component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -2,26 +2,35 @@ import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../store/app.state';
 import { Observable } from 'rxjs';
-import { CartItem } from '../../models/product.model';
+import { CartItem, OrderItem } from '../../models/product.model';
 import { AsyncPipe } from '@angular/common';
 import { selectCart } from '../../store/cart/selectors/cart.selectors';
 import { removeFromCart } from '../../store/cart/actions/cart.actions';
+import { placeOrder } from '../../store/order/actions/order.actions';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
   imports: [AsyncPipe],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.sass'
+  styleUrl: './cart.component.sass',
 })
 export class CartComponent {
-  cart$: Observable<CartItem[]>
+  cart$: Observable<CartItem[]>;
   constructor(private store: Store<AppState>) {
     this.cart$ = this.store.select(selectCart);
-    this.store.select(selectCart).subscribe(cart => localStorage.setItem('cart', JSON.stringify(cart)))
+    this.store
+      .select(selectCart)
+      .subscribe((cart) => localStorage.setItem('cart', JSON.stringify(cart)));
   }
 
   removeFromCart(cartItem: CartItem) {
-    this.store.dispatch(removeFromCart({cartItem}))
+    this.store.dispatch(removeFromCart({ cartItem }));
+  }
+
+  placeOrder() {
+    this.cart$.subscribe((order: OrderItem[]) => {
+      this.store.dispatch(placeOrder({ order }));
+    });
   }
 }
