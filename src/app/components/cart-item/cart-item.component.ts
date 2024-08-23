b18cd965dd1fbe9f96fd2c48c8966b1a0bf688c9
kewablay <kojokewajunior123@gmail.com>
2024-08-23 11:15:06 +0000

    added base list item import

--- src/app/components/cart-item/cart-item.component.ts
+++ src/app/components/cart-item/cart-item.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BaseListItemComponent } from "../base-list-item/base-list-item.component";
 
 @Component({
   selector: 'app-cart-item',
   standalone: true,
-  imports: [],
+  imports: [BaseListItemComponent],
   templateUrl: './cart-item.component.html',
   styleUrl: './cart-item.component.sass'
 })
