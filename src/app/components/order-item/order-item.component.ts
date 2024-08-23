62eaf0060b897c00d9cffc11b1858c5874c41611
kewablay <kojokewajunior123@gmail.com>
2024-08-23 11:15:30 +0000

    added import for base list component

--- src/app/components/order-item/order-item.component.ts
+++ src/app/components/order-item/order-item.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BaseListItemComponent } from "../base-list-item/base-list-item.component";
 
 @Component({
   selector: 'app-order-item',
   standalone: true,
-  imports: [],
+  imports: [BaseListItemComponent],
   templateUrl: './order-item.component.html',
   styleUrl: './order-item.component.sass'
 })
