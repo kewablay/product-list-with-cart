ba0772ea59234a4fe7cbb43053241cdce8ba051e
kewablay <kojokewajunior123@gmail.com>
2024-08-23 09:38:16 +0000

    provided my order state in the app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,6 +9,7 @@ import { productsReducer } from './store/products/reducers/products.reducers';
 import { ProductsEffects } from './store/products/effects/products.effects';
 import { provideHttpClient } from '@angular/common/http';
 import { cartReducer } from './store/cart/reducers/cart.reducers';
+import { orderReducer } from './store/order/reducers/order.reducers';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -23,6 +24,10 @@ export const appConfig: ApplicationConfig = {
     provideState({
       name: 'cart',
       reducer: cartReducer,
+    }),
+    provideState({
+      name: 'order',
+      reducer: orderReducer,
     }),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
   ],
