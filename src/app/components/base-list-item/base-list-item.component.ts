561252dd1aa64239052439cd9c0fa4c7048f56b3
kewablay <kojokewajunior123@gmail.com>
2024-08-23 09:38:34 +0000

    added base list item component

---
+++ src/app/components/base-list-item/base-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './base-list-item.component.html',
+  styleUrl: './base-list-item.component.sass'
+})
+export class BaseListItemComponent {
+
+}
