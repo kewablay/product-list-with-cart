a8a32437682f2a9ce1b5c07e5d9698dae204808a
kewablay <kojokewajunior123@gmail.com>
2024-08-24 23:41:59 +0000

    added order item data and currency pipe

--- src/app/components/order-item/order-item.component.ts
+++ src/app/components/order-item/order-item.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { BaseListItemComponent } from "../base-list-item/base-list-item.component";
+import { OrderItem } from '../../models/product.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-order-item',
   standalone: true,
-  imports: [BaseListItemComponent],
+  imports: [BaseListItemComponent, CurrencyPipe],
   templateUrl: './order-item.component.html',
   styleUrl: './order-item.component.sass'
 })
 export class OrderItemComponent {
+  @Input() orderItem!: OrderItem;
 
 }
