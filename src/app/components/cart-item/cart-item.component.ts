99489b485da3b74c78aae1e97b7d14fd32e9a886
kewablay <kojokewajunior123@gmail.com>
2024-08-24 23:38:57 +0000

    added currency pipe to cart item

--- src/app/components/cart-item/cart-item.component.ts
+++ src/app/components/cart-item/cart-item.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { BaseListItemComponent } from "../base-list-item/base-list-item.component";
 import { CartItem } from '../../models/product.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-cart-item',
   standalone: true,
-  imports: [BaseListItemComponent],
+  imports: [BaseListItemComponent, CurrencyPipe],
   templateUrl: './cart-item.component.html',
   styleUrl: './cart-item.component.sass'
 })
