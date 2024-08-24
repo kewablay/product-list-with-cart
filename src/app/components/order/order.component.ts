c4806b96fcf8c4d6f041a28bb81421406dbbc502
kewablay <kojokewajunior123@gmail.com>
2024-08-24 23:41:20 +0000

    updated order component to handle closing of modal after starting a new order

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../store/app.state';
 import { Observable } from 'rxjs';
@@ -6,16 +6,20 @@ import { OrderItem } from '../../models/product.model';
 import { selectOrder } from '../../store/order/selectors/order.selectors';
 import { AsyncPipe } from '@angular/common';
 import { clearCart } from '../../store/cart/actions/cart.actions';
+import { CartItemComponent } from "../cart-item/cart-item.component";
+import { OrderItemComponent } from "../order-item/order-item.component";
 
 @Component({
   selector: 'app-order',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, CartItemComponent, OrderItemComponent],
   templateUrl: './order.component.html',
   styleUrl: './order.component.sass',
 })
 export class OrderComponent {
   order$: Observable<OrderItem[]>;
+  @Output() closeModal = new EventEmitter<void>()
+
   constructor(private store: Store<AppState>) {
     this.order$ = this.store.select(selectOrder);
 
@@ -27,5 +31,6 @@ export class OrderComponent {
   startNewOrder() {
     // close order modal
     this.store.dispatch(clearCart());
+    this.closeModal.emit();
   }
 }
