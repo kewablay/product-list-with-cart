e73d769d5c9af246f9f7602a32031b2e8657e17f
kewablay <kojokewajunior123@gmail.com>
2024-08-25 01:34:52 +0000

    added cart total logics

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -3,39 +3,55 @@ import { Store } from '@ngrx/store';
 import { AppState } from '../../store/app.state';
 import { Observable } from 'rxjs';
 import { CartItem, OrderItem } from '../../models/product.model';
-import { AsyncPipe } from '@angular/common';
-import { selectCart } from '../../store/cart/selectors/cart.selectors';
+import { AsyncPipe, CurrencyPipe } from '@angular/common';
+import {
+  selectCart,
+  selectCartTotal,
+} from '../../store/cart/selectors/cart.selectors';
 import { removeFromCart } from '../../store/cart/actions/cart.actions';
 import { placeOrder } from '../../store/order/actions/order.actions';
 import { CartItemComponent } from '../cart-item/cart-item.component';
 import { ModalService } from '../../services/modalService/modal.service';
 import { ModalComponent } from '../modal/modal.component';
 import { OrderComponent } from '../order/order.component';
+import { LocalStorageService } from '../../services/localStorageService/local-storage.service';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [AsyncPipe, CartItemComponent, ModalComponent, OrderComponent],
+  imports: [
+    AsyncPipe,
+    CurrencyPipe,
+    CartItemComponent,
+    ModalComponent,
+    OrderComponent,
+  ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.sass',
 })
 export class CartComponent {
   cart$: Observable<CartItem[]>;
+  cartTotal$: Observable<number>;
   isModalOpen: boolean = false;
   constructor(
     private store: Store<AppState>,
-    private modalService: ModalService
+    private modalService: ModalService,
+    private localStorageService: LocalStorageService
   ) {
     this.cart$ = this.store.select(selectCart);
+    this.cartTotal$ = this.store.select(selectCartTotal);
+
     this.store
       .select(selectCart)
       .subscribe((cart) => localStorage.setItem('cart', JSON.stringify(cart)));
+
+    this.store
+      .select(selectCartTotal)
+      .subscribe((cartTotal) =>
+        this.localStorageService.setItem('cartTotal', cartTotal)
+      );
     // check whether the order modal is open
     this.isModalOpen = this.modalService.isModalOpen('orderModal');
-    console.log(
-      'is order modal open: ',
-      this.modalService.isModalOpen('orderModal')
-    );
   }
 
   removeFromCart(cartItem: CartItem) {
@@ -47,10 +63,7 @@ export class CartComponent {
   placeOrder() {
     this.modalService.openModal('orderModal');
     this.isModalOpen = this.modalService.isModalOpen('orderModal');
-    console.log(
-      'is order modal open: ',
-      this.modalService.isModalOpen('orderModal')
-    );
+    
     this.cart$.subscribe((order: OrderItem[]) => {
       this.store.dispatch(placeOrder({ order }));
     });
