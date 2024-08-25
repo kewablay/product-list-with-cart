e65d6876a538f97ef45335f4bc9238b51ee2078c
kewablay <kojokewajunior123@gmail.com>
2024-08-25 15:43:48 +0000

    fixed page scroll when modal is active

--- src/app/services/modalService/modal.service.ts
+++ src/app/services/modalService/modal.service.ts
@@ -7,14 +7,25 @@ export class ModalService {
   // if app ever needs other modals it will go into the modals object
   private modals: { [key: string]: boolean } = {};
 
+   // Toggle scroll lock based on whether any modal is open
+   private toggleScrollLock() {
+    const isAnyModalOpen = Object.values(this.modals).some((isOpen) => isOpen);
+    if (isAnyModalOpen) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+  }
   constructor() {}
 
   openModal(modalName: string) {
     this.modals[modalName] = true;
+    this.toggleScrollLock();
   }
 
   closeModal(modalName: string) {
     this.modals[modalName] = false;
+    this.toggleScrollLock();
   }
 
   isModalOpen(modalName: string) {
