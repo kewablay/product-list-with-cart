afe832aa8d2911f1ea7f0670e6fa270baf237145
kewablay <kojokewajunior123@gmail.com>
2024-08-25 14:10:19 +0000

    added logics for handling descrease and increase quantity

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,6 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Dessert } from '../../models/product.model';
+import { CartItem, Dessert } from '../../models/product.model';
 import { CurrencyPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { AppState } from '../../store/app.state';
+import { Store } from '@ngrx/store';
+import { selectCart } from '../../store/cart/selectors/cart.selectors';
 
 @Component({
   selector: 'app-product-item',
@@ -11,10 +15,33 @@ import { CurrencyPipe } from '@angular/common';
 })
 export class ProductItemComponent {
   @Output() addToCart = new EventEmitter<any>();
+  @Output() decreaseQuantity = new EventEmitter<any>();
   @Input() product!: Dessert;
+  cart$!: Observable<CartItem[]>;
+  isInCart: boolean = false;
+  quantity: number = 0;
+
+  constructor(private store: Store<AppState>) {
+    this.cart$ = this.store.select(selectCart);
+
+    this.cart$.subscribe((cart) => {
+      const itemInCart = cart.find((item) => item.name === this.product?.name);
+      if (itemInCart) {
+        this.isInCart = true;
+        this.quantity = itemInCart.quantity;
+      } else {
+        this.isInCart = false;
+        this.quantity = 0;
+      }
+    });
+  }
 
 
-  handleAddToCart(product:Dessert) {
+  handleAddToCart(product: Dessert) {
     this.addToCart.emit(product);
+  }
+
+  handleDecreaseQuantity(product: Dessert) {
+    this.decreaseQuantity.emit(product.name);
   }
 }
